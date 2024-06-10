c71d8781854e9395b887c4bd78498432c38daea0
fe-ercg <mabfher@gmail.com>
2024-06-10 10:56:52 -0500

    feat: showin data in HTML

--- src/main.js
+++ src/main.js
@@ -4,5 +4,4 @@ import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
 const render = document.querySelector("#box");
-render.innerHTML= renderItems.mostrarValores(data);
 console.log(example, renderItems(data), data);
