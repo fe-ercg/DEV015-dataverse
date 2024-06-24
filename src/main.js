076366f8558e1def677074723e9eb2e910603a5b
fe-ercg <mabfher@gmail.com>
2024-06-24 09:43:22 -0500

    fix: comentarios main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import { renderItems } from "./view.js";
 
 import data from "./data/dataset.js";
 
-
-
 // Trae la info de la funcion render items
 const render = document.querySelector("#root");
 render.appendChild(renderItems(data));
