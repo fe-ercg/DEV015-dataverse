45ebb009e0716ea73cd1d8884d1efc8b5b14c8a7
fe-ercg <mabfher@gmail.com>
2024-06-24 16:52:20 -0500

    feat: styles CSS an  view.js modified

--- src/view.js
+++ src/view.js
@@ -10,33 +10,30 @@ export const renderItems = (data) => {
     li.setAttribute('itemtype', 'Spiderverse')
     li.setAttribute('itemscope', '');
     li.setAttribute('data-id', data.id)
-    li.setAttribute('data-id', data.id)
     li.classList.add('item-lista')
     ul.appendChild(li);
-    
 
     //IMAGEN
-
     const personajeImagen = document.createElement('div');
     personajeImagen.style.backgroundImage = `url(${data.imageUrl})`;
     personajeImagen.classList.add('image')
     personajeImagen.setAttribute('alt', data.name);
     li.appendChild(personajeImagen);
     
-    
     //TEXTO-------------
     const dl = document.createElement('dl');
     li.appendChild(dl);
-
+    
     //NAME
     const dtNombre = document.createElement('dt');
     dl.appendChild(dtNombre);
+    dtNombre.classList.add('name')
     dtNombre.innerHTML = data.name;
-    
     
     //DESCRIPCION - CORTA 
     const dtDescripcion = document.createElement('dt');
     dl.appendChild(dtDescripcion);
+    dtDescripcion.classList.add('descripcion')
     dtDescripcion.innerHTML = data.shortDescription;
 
     //DIV para FACTS
@@ -47,29 +44,22 @@ export const renderItems = (data) => {
     //FACTS - GENERO
     const dtGender = document.createElement('dt');
     facts.appendChild(dtGender);
-    dtGender.innerHTML = 'Genero: '+ data.facts.gender;
-    
+    dtGender.innerHTML = '<span class="label">Genero: </span><span class="value">' + data.facts.gender + '</span>';
     
     //FACTS - ESPECIE
     const dtSpecies = document.createElement('dt');
     facts.appendChild(dtSpecies);
-    dtSpecies.innerHTML = 'Especie: '+ data.facts.species;
-    
- 
-    
+    dtSpecies.innerHTML = '<span class="label">Especie: </span><span class="value">' + data.facts.species + '</span>';
+        
     //FACTS - EDAD
     const dtAge = document.createElement('dt');
     facts.appendChild(dtAge);
-    dtAge.innerHTML = 'Edad: '+ data.facts.age;
-    
-  
-    
+    dtAge.innerHTML = '<span class="label">Edad: </span><span class="value">' + data.facts.age + '</span>';
+
     //FACTS - CIUDAD
     const dtCity = document.createElement('dt');
     facts.appendChild(dtCity);
-    dtCity.innerHTML = 'Ciudad: '+ data.facts.city;
-    
-   
+    dtCity.innerHTML = '<span class="label"> Ciudad: </span><span class="value">' + data.facts.city + '</span>';
   }
 
   data.forEach(crearElemento);
